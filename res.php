d6e2f1d37f13fe88debcf413e95d6cb4506280d1
boyuchen0224 <boyuchen0224@gmail.com>
2019-05-27 00:05:03 +0800

    ver2 random response

--- res.php
+++ res.php
@@ -1,12 +1,27 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        $path = "test_up.jpg";
+        $num = rand(1,10);
+        if($num < 5){
+            $path = "test_up.jpg";
         $name = "饅頭";
         $count = "10個";
         $tmp = "27度";
         $hum = "40%";
         $mat = "過熟";
-        $response = "{\"name\":\"".$name."\",\"count\":\"".$count."\",\"tmp\":\"".$tmp."\",\"hum\":\"".$hum."\",\"mat\":\"".$mat."\"}";
+        $response = "{\"name\":\"".$name."\",\"count\":\"".$count."\",\"tmp\":\"".$tmp."\",\"hum\":\"".$hum."\",\"mat\":\"".$mat."\",\"path\":\"".$path."\"}";
         echo $response;
+        $num = 0;
+        }
+        else{
+            $path = "meow.jpg";
+        $name = "妞妞";
+        $count = "3個";
+        $tmp = "24度";
+        $hum = "37%";
+        $mat = "微熟";
+        $response = "{\"name\":\"".$name."\",\"count\":\"".$count."\",\"tmp\":\"".$tmp."\",\"hum\":\"".$hum."\",\"mat\":\"".$mat."\",\"path\":\"".$path."\"}";
+        echo $response;
+        $num = 1;
+        }
     }
 ?>
